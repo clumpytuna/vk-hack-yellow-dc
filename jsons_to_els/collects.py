8893ce059448409f0176cfb23e5e468f67769c67
Ivan Leskin <leskin.in@arenadata.io>
2019-09-28 22:59:23 +0300

    Update parsers

--- jsons_to_els/collects.py
+++ jsons_to_els/collects.py
@@ -9,7 +9,7 @@ URL_ELS_WITH_FORMAT = 'http://localhost:9200/{}/_doc/{}'
 
 
 def send_json_to_els(index: str, data_id: str, data: str):
-    return requests.post(URL_ELS_WITH_FORMAT.format(index, data_id), data=data, headers={'Content-Type': 'application/json'})
+    return requests.post(URL_ELS_WITH_FORMAT.format(index, data_id), data=data.encode('utf-8'), headers={'Content-Type': 'application/json'})
 
 
 def nonize_field(data: dict, key: str):
@@ -19,7 +19,7 @@ def nonize_field(data: dict, key: str):
 
 def main():
     if len(sys.argv) < 3:
-        print("Syntax: ./events.py <file> <index_in_elastic>")
+        print("Syntax: ./collects.py <file> <index_in_elastic>")
         return
 
     with open(sys.argv[1], 'r') as f:
@@ -30,18 +30,18 @@ def main():
     jsons = json.loads(raw)
     for data_id in jsons.keys():
         data = jsons[data_id]
+        object_result = {
+            'path': data['path'],
+            'name': data['name']['ru'],
+            'text': data['text']['ru'],
+            'img': data['circ_img']['id01'],
+        }
 
-        # Special actions
-        del data['lbmore']
-        del data['lbhide']
-        del data['searcha']
+        object_dumped = json.dumps(object_result, ensure_ascii=False)
+        print("Object {}\n{}".format(data_id, object_dumped))
 
-        # Normalization
-        for field in data.keys():
-            nonize_field(data, field)
-
-        response = send_json_to_els(index, data_id, json.dumps(data))
-        print("Result for #{}: Code {}, Content {}".format(data_id, response.status_code, str(response.content)))
+        response = send_json_to_els(index, data_id, object_dumped)
+        print("Add to Elastic: Code {}, Content {}".format(data_id, response.status_code, str(response.content)))
 
 
 if __name__ == '__main__':
--- jsons_to_els/events.py
+++ jsons_to_els/events.py
@@ -9,7 +9,7 @@ URL_ELS_WITH_FORMAT = 'http://localhost:9200/{}/_doc/{}'
 
 
 def send_json_to_els(index: str, data_id: str, data: str):
-    return requests.post(URL_ELS_WITH_FORMAT.format(index, data_id), data=data, headers={'Content-Type': 'application/json'})
+    return requests.post(URL_ELS_WITH_FORMAT.format(index, data_id), data=data.encode('utf-8'), headers={'Content-Type': 'application/json'})
 
 
 def nonize_field(data: dict, key: str):
@@ -30,16 +30,24 @@ def main():
     jsons = json.loads(raw)
     for data_id in jsons.keys():
         data = jsons[data_id]
+        object_result = {
+            'path': data['path'],
+            'img': data['img_header']['pc'],
+            'dateBegin': data['dateBegin'],
+            'dateEnd': data['dateEnd'],
+            'name': data['name']['ru'],
+            'text': data['text']['ru'],
+            'price': data['price']['ru'],
+            'audioguide': data['audioguide']['ru'],
+            'building': data['building']['0'],
+            'ticket': data['ticket'],
+        }
 
-        # Special operations
-        del data['searcha']
+        object_dumped = json.dumps(object_result, ensure_ascii=False)
+        print("Object {}\n{}".format(data_id, object_dumped))
 
-        # Normalization
-        for field in data.keys():
-            nonize_field(data, field)
-
-        response = send_json_to_els(index, data_id, json.dumps(data))
-        print("Result for #{}: Code {}, Content {}".format(data_id, response.status_code, str(response.content)))
+        response = send_json_to_els(index, data_id, object_dumped)
+        print("Add to Elastic: Code {}, Content {}".format(data_id, response.status_code, str(response.content)))
 
 
 if __name__ == '__main__':
