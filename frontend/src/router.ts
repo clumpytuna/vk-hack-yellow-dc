d59720521b44c91d2cf7c39bd86c53a93480dd51
Dmitry Murzin <diraria@yandex.ru>
2019-09-28 20:37:41 +0300

    fixes

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -9,15 +9,8 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
-    },
+    { path: '/', name: 'home', component: Home },
+    { path: '/about', name: 'about', component: About },
+    { path: '*', redirect: '/' },
   ],
 });
